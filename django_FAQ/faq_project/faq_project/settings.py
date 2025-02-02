f08792e4e90174e063910fa2c453b02c9f1dd625
Prasanthisandaka2005 <prasanthisandaka2005@gmail.com>
2025-02-02 19:11:03 +0530

    modified settings

--- django_FAQ/faq_project/faq_project/settings.py
+++ django_FAQ/faq_project/faq_project/settings.py
@@ -63,17 +63,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
-
-
 ROOT_URLCONF = 'faq_project.urls'
 
 TEMPLATES = [
